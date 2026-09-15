a6b35abcb67cfcfa9238f2478bade0078a5643c0
Lakshanya Lenin <lakshanyalenin01062008@gmail.com>
2026-09-15 11:30:47 +0530

    Add Week 6 M3 Employee Profile

---
+++ Week6/M3_EmployeeProfile/Employee.java
@@ -0,0 +1,37 @@
+class Employee {
+
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+
+    // Constructor for permanent employee
+    public Employee(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+
+    // Constructor for intern
+    public Employee(String empId, String empName) {
+        this(empId, empName, 0);
+        this.isIntern = true;
+    }
+
+    // Method to print employee profile
+    public void printProfile() {
+        System.out.println(empId + " | " + empName + " | Rs "
+                + salary + " | Intern: " + isIntern);
+    }
+
+    public static void main(String[] args) {
+
+        Employee permanentEmployee = new Employee("E-101", "Divya", 65000);
+
+        Employee internEmployee = new Employee("E-102", "Arjun");
+
+        permanentEmployee.printProfile();
+        internEmployee.printProfile();
+    }
+}
