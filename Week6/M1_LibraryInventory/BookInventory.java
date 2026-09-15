0a3842b4e9f576f2832d1d30b31b549666cae7c2
Lakshanya Lenin <lakshanyalenin01062008@gmail.com>
2026-09-15 11:25:08 +0530

    Add Week 6 M1 Library Inventory

---
+++ Week6/M1_LibraryInventory/BookInventory.java
@@ -0,0 +1,36 @@
+class BookInventory {
+
+    String title;
+    String author;
+    int copiesAvailable;
+
+    // Constructor
+    BookInventory(String title, String author, int copiesAvailable) {
+        this.title = title;
+        this.author = author;
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    // Instance method
+    void printEntry() {
+        System.out.println(title + " by " + author + " - "
+                + copiesAvailable + " copies available");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating four BookInventory objects
+        BookInventory book1 = new BookInventory("Clean Code", "Robert C. Martin", 3);
+        BookInventory book2 = new BookInventory("Effective Java", "Joshua Bloch", 5);
+        BookInventory book3 = new BookInventory("Refactoring", "Martin Fowler", 0);
+        BookInventory book4 = new BookInventory("Design Patterns", "GoF", 2);
+
+        // Storing objects in an array
+        BookInventory[] books = { book1, book2, book3, book4 };
+
+        // Printing each book
+        for (BookInventory book : books) {
+            book.printEntry();
+        }
+    }
+}
