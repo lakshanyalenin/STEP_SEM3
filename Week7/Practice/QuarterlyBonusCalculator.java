2b6c01e20edd33608acfc79642b76c73211a6d8e
Lakshanya Lenin <lakshanyalenin01062008@gmail.com>
2026-09-23 21:21:22 +0530

    Add Week 7 Practice Q3 Quarterly Bonus Calculator

---
+++ Week7/Practice/QuarterlyBonusCalculator.java
@@ -0,0 +1,89 @@
+abstract class StaffMember {
+
+    private double baseSalary;
+    protected double bonusRate;
+
+    public StaffMember(double baseSalary) {
+        this(baseSalary, 0.10);
+    }
+
+    public StaffMember(double baseSalary, double bonusRate) {
+        this.baseSalary = baseSalary;
+        this.bonusRate = bonusRate;
+    }
+
+    public abstract double calculateBonus();
+
+    public double getSalary() {
+        return baseSalary;
+    }
+
+    public void setSalary(double baseSalary) {
+        if (baseSalary >= 0) {
+            this.baseSalary = baseSalary;
+        }
+    }
+}
+
+interface Auditable {
+    String auditRecord();
+}
+
+class TeamLead extends StaffMember implements Auditable {
+
+    private int teamSize;
+
+    public TeamLead(double baseSalary, int teamSize) {
+        super(baseSalary);
+        this.teamSize = teamSize;
+    }
+
+    public TeamLead(double baseSalary, double bonusRate, int teamSize) {
+        super(baseSalary, bonusRate);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public double calculateBonus() {
+        return getSalary() * bonusRate;
+    }
+
+    @Override
+    public String auditRecord() {
+        return "TeamLead audit: " + teamSize
+                + " team members, salary $" + getSalary();
+    }
+}
+
+public class QuarterlyBonusCalculator {
+
+    static String getAuditIfApplicable(StaffMember s) {
+
+        if (s instanceof Auditable) {
+            Auditable auditable = (Auditable) s;
+            return auditable.auditRecord();
+        }
+
+        return "No audit required";
+    }
+
+    public static void main(String[] args) {
+
+        TeamLead t = new TeamLead(60000, 5);
+
+        System.out.println(t.calculateBonus());
+
+        TeamLead t2 = new TeamLead(60000, 0.20, 5);
+
+        System.out.println(t2.calculateBonus());
+
+        t.setSalary(-5000);
+
+        System.out.println("Salary after invalid update: " + t.getSalary());
+
+        StaffMember ref = t;
+        // Upcasting: TeamLead stored as StaffMember
+
+        System.out.println(getAuditIfApplicable(ref));
+    }
+}
